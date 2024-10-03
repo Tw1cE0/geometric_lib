a5ac406c191d41b0bd89e9862322546d43102801
Artemii Safonov <artemii.e.safonov@gmail.com>
2024-10-03 14:12:26 +0300

    Добавлена документация в файл square

--- square.py
+++ square.py
@@ -1,7 +1,33 @@
 
 def area(a):
+    """
+    Возвращает площадь квадрата.
+
+    Args:
+        a (int): Длина стороны квадрата.
+
+    Returns:
+        int: Площадь квадрата.
+
+    Example:
+        >>> area(4)
+        16
+    """
     return a * a
 
 
 def perimeter(a):
+    """
+    Возвращает периметр квадрата.
+
+    Args:
+        a (int): Длина стороны квадрата.
+
+    Returns:
+        int: Периметр квадрата.
+
+    Example:
+        >>> perimeter(4)
+        16
+    """
     return 4 * a
