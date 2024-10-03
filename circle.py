dd3916a291892f73331029732fa6fc7064ef82ab
Artemii Safonov <artemii.e.safonov@gmail.com>
2024-10-03 14:10:37 +0300

    Добавленио документирование в файл circle

--- circle.py
+++ circle.py
@@ -1,10 +1,33 @@
 import math
 
-
 def area(r):
+    """
+    Возвращает площадь круга.
+
+    Args:
+        r (float): Радиус круга.
+
+    Returns:
+        float: Площадь круга.
+
+    Example:
+        >>> area(5)
+        78.53981633974483
+    """
     return math.pi * r * r
 
-
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Возвращает периметр круга.
 
+    Args:
+        r (float): Радиус круга.
+
+    Returns:
+        float: Периметр круга.
+
+    Example:
+        >>> perimeter(5)
+        31.41592653589793
+    """
+    return 2 * math.pi * r
